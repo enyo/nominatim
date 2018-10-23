07b7afa242917f184a36a6bd394ddbb535c7ada7
Briar Bowser <briarbear7@gmail.com>
2018-10-22 21:03:03 -0500

    Updated to Dart 2 and the latest XML package. Updated linting errors.

--- lib/search_results.dart
+++ lib/search_results.dart
@@ -1,13 +1,12 @@
 part of nominatim;
 
-/**
- * Holds information about an OpenStreetMap Nominatim SearchResult and
- * the list of places returned.
- * 
- * An instance of this class is returned when using [Nominatim.search].
- */
+///
+/// Holds information about an OpenStreetMap Nominatim SearchResult and
+/// the list of places returned.
+/// 
+/// An instance of this class is returned when using [Nominatim.search].
+///
 class SearchResults {
-  
   final List<Place> places;
 
   final String timestamp;
@@ -20,20 +19,17 @@ class SearchResults {
   
   final bool polygon;
   
-  /**
-   * Creates a new [SearchResult] with given attributes.
-   */
+  /// Creates a new [SearchResults] with given attributes.
   SearchResults({ this.timestamp, this.attribution, this.moreUrl, this.queryString, this.polygon, this.places });
   
   String toString() {
     var string = new List();
-    string.add("SearchResult (${timestamp})");
+    string.add("SearchResults ($timestamp)");
     
     this.places.forEach((place) {
       string.add("  - ${place.displayName}");
     });
     return string.join("\n");
   }
-  
 }
 
